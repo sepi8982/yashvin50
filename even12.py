1f9fd28e224e8f44c4a82fd79875b707131788c8
sepi8982 <pathaksapna27@gmail.com>
2022-05-05 02:35:51 -0700

    adding file

---
+++ even12.py
@@ -0,0 +1,33 @@
+'''s="12345"
+if (s.split and s%2==0):
+    print("even")
+    else:
+    print("odd")
+'''
+ ######################################
+
+'''n="sapna"
+if n==n.upper():
+ print(n.lower())
+else:
+ print(n.upper())
+
+ s="sapna"
+ if "a"<= s <="z"
+ '''
+
+######################################
+'''n=2020
+if n%400==0 or n%100!=0 :
+ print("leap year")
+else:
+ print("not a leap year")
+'''
+#############################
+
+n={"sapna":1,"sapna1":2,"sapna3":3}
+if len(n)%2==0 :
+  print(n)
+else:
+ n.update(n=4)
+ print(n) 
